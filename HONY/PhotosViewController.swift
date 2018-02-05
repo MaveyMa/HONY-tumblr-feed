feb740adace59b89cc4db90258d124ca7c7e5d86
Mavey Ma <maveyma@gmail.com>
2018-02-04 23:43:58 -0800

    Part 2 should be here

--- HONY/PhotosViewController.swift
+++ HONY/PhotosViewController.swift
@@ -10,12 +10,11 @@ import UIKit
 
 class PhotosViewController: UIViewController {
   
-  @IBOutlet weak var tableView: UITableView!
   var posts: [[String: Any]] = []
-  var refreshControl: UIRefreshControl!
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    
     // Network request snippet
     let url = URL(string: "https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV")!
     let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
@@ -25,14 +24,13 @@ class PhotosViewController: UIViewController {
         print(error.localizedDescription)
       } else if let data = data,
         let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
+        print(dataDictionary)
         
         // Get the dictionary from the response key
         let responseDictionary = dataDictionary["response"] as! [String: Any]
         // Store the returned array of dictionaries in our posts property
         self.posts = responseDictionary["posts"] as! [[String: Any]]
-        
-        // Reload the table view
-        self.tableView.reloadData()
+        // TODO: Reload the table view
       }
     }
     task.resume()
@@ -43,15 +41,5 @@ class PhotosViewController: UIViewController {
     // Dispose of any resources that can be recreated.
   }
   
-  
-  /*
-   // MARK: - Navigation
-   
-   // In a storyboard-based application, you will often want to do a little preparation before navigation
-   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-   // Get the new view controller using segue.destinationViewController.
-   // Pass the selected object to the new view controller.
-   }
-   */
-  
+
 }
